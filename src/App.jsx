ca45c1a33b732ccac2e86b768273c895ef471f08
LuisAndreLuqueQ <l.luque.work@gmail.com>
2026-06-29 10:20:52 -0500

    fix: Removerbarra de navegacion faltante y las importaciones de PrductPage no usadas

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,18 @@
-import { Routes, Route } from 'react-router-dom'
-import { Home } from './pages/Home'
-import { ProductPage } from './pages/ProductPage'
-import { ProductDetail } from './components/ProductDetail'
-import { NotFound } from './pages/NotFound'
-
-import { Navbar } from './components/Navbar'
-
+import { Routes, Route } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { ProductDetail } from "./components/Product Detail";
+import { NotFound } from "./pages/NotFound";
 
 function App() {
   return (
     <>
-        <Routes>
-
-          <Route path="/" element={<Home />} />
-          <Route path="/products/:id" element={<ProductDetail/>} />
-          <Route path="*" element={<NotFound />} />
-          
-        </Routes>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products/:id" element={<ProductDetail />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
-
-
+export default App;
--- src/hooks/useFetchProducts.js
+++ src/hooks/useFetchProducts.js
@@ -1,28 +1,28 @@
-  import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
-  export const useFetchProducts = (url) => {
-    const [data, setData] = useState(null);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
+export const useFetchProducts = (url) => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
-    useEffect(() => {
-      const fetchData = async () => {
-        setLoading(true);
-        setError(null);
-        try {
-          const res = await fetch(url);
-          if (!res.ok) throw new Error('Error al obtener los datos');
-          const json = await res.json();
-          setData(json);
-        } catch (err) {
-          setError(err.message);
-        } finally {
-          setLoading(false);
-        }
-      };
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      setError(null);
+      try {
+        const res = await fetch(url);
+        if (!res.ok) throw new Error("Error al obtener los datos");
+        const json = await res.json();
+        setData(json);
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    };
 
-      fetchData();
-    }, [url]);
+    fetchData();
+  }, [url]);
 
-    return { data, loading, error };
-  };
+  return { data, loading, error };
+};
