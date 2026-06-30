c1459592fb0146fdcdd2733120b72269978791cb
LuisAndreLuqueQ <l.luque.work@gmail.com>
2026-06-29 23:41:34 -0500

    Feat: barra de navegacion con logotipo, enlaces a categorias, barra de busqueda e icono de carrito

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import { Routes, Route } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { ProductDetail } from "./components/ProductDetail";
 import { NotFound } from "./pages/NotFound";
+import { Navbar } from "./components/Navbar";
 
 function App() {
   return (
     <>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/products/:id" element={<ProductDetail />} />
